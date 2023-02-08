d686a706ab963dad62002dc07342b3267375e2df
R-twitzy <ramadhanmaulana446@gmail.com>
2023-02-08 07:42:34 +0700

    08 Feb 2023

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <col>
+        
+        </col>
+        <img src={require("./deazard.JPG")} className="App-logo" alt="logo" />
+        <br/>
+        <br/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          deazard berputar-putar tanpa henti
         </p>
         <a
           className="App-link"
